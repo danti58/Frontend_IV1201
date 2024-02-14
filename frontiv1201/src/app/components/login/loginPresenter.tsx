6bceff81119d1e1a61df3cc86b8689ed788dc313
danti58 <dante.nilsson28@gmail.com>
2024-02-14 11:03:02 +0100

    Revert "Made the redux store"
    This reverts commit 33cba35db87bbb8dc51c6d9548aff41d522c5915.

--- frontiv1201/src/app/components/login/loginPresenter.tsx
+++ frontiv1201/src/app/components/login/loginPresenter.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import { loginPerson, LoginData } from '@/app/api';
 import LoginView from './loginView';
-import { setAuthData } from '@/app/redux/actions';
-import { useDispatch } from 'react-redux';
-
 
 function LoginPresenter() {
-
-    const dispatch = useDispatch();
-
   const [loginData, setLoginData] = React.useState<LoginData>({
     username: '',
     password: '',
   });
 
   function onLoginSuccess(response: any) {
-      // print response json:
-      console.log('Hej');
-      console.log('Login success:', response);
+    // print response json:
+    console.log('Login success:', response);
   }
 
     function onLoginFail(error: any) {
@@ -28,22 +21,13 @@ function LoginPresenter() {
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Prevent the default form submission behavior
     try {
-        const response = await loginPerson(loginData);
-        handleApiData(response);
-        //dispatch(setAuthData(response.data.token, response.data.user.role_id, response.data.user.username));
+      const response = await loginPerson(loginData);
       onLoginSuccess(response);
     } catch (error) {
       onLoginFail(error);
     }
   };
 
-    const handleApiData = (responseData) => {
-        const token = responseData.token;
-        const role_id = responseData.user.role_id;
-        const username = responseData.user.username;
-
-        dispatch(setAuthData(token, role_id, username));
-    };
 
   return <LoginView loginData={loginData} setLoginData={setLoginData} handleLogin={handleLogin} />;
 };
--- frontiv1201/src/pages/_app.tsx
+++ frontiv1201/src/pages/_app.tsx
@@ -1,20 +1,9 @@
 // src/pages/_app.tsx or pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '../app/globals.css'; // Adjust the path to your globals.css file
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import combineReducers from '@/app/redux/reducers';
-
-
-const store = createStore(combineReducers);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-    return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
-
-    );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
